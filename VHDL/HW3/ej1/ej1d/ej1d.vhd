70b65c1a8967695f995381baecc1bba2dd83f2c6
agustinsilva447 <agustinsilva447@gmail.com>
2021-04-30 16:21:01 -0300

    vhdl package

--- VHDL/HW3/ej1/ej1d/ej1d.vhd
+++ VHDL/HW3/ej1/ej1d/ej1d.vhd
@@ -4,20 +4,20 @@ use ieee.std_logic_1164.all;
 entity ej1d is
 port
 (
-	 a,b,cin: in std_logic;
-	  s,cout: out std_logic
+	 entradas: in std_logic_vector(0 to 2);
+	  salidas: out std_logic_vector(0 to 1)
 );
 end ej1d;
 
 architecture sumador_completo of ej1d is
 begin
-	with (a, b, cin) select
-		(s,cout) <= "00" when "000",
-						"01" when "001",
-						"01" when "010",
-						"10" when "011",
-						"01" when "100",
-						"10" when "101",
-						"10" when "110",
-						"11" when "111";
+	with entradas select
+		salidas <= "00" when "000",
+					  "01" when "001",
+					  "01" when "010",
+					  "10" when "011",
+					  "01" when "100",
+					  "10" when "101",
+					  "10" when "110",
+					  "11" when "111";
 end sumador_completo;
