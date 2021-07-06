3856e2185dfed8313961e16eaa4cd491e381ca73
agustinsilva447 <agustinsilva447@gmail.com>
2021-07-06 14:06:34 -0300

    hopfield routing

--- paper_routing/paper_v2.py
+++ paper_routing/paper_v2.py
@@ -2,24 +2,53 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-w = np.array([[0,   70,  0,   30,  0,   0],
-              [70,  0,   100, 70,  110, 0],
-              [0,   100, 0,   120, 40,  80],
-              [30,  70,  120, 0,   90,  0],
-              [0,   110, 40,  90,  0,   50],
-              [0,   0,   80,  0,   50,  0]])
+def dijkstra_sp(net, paquetes, C_xi):
+    source = []
+    destin = []
+    paths = []
+    for i in range(paquetes):
+        rng = np.random.default_rng()
+        s, d = rng.choice(C_xi.shape[0], size=2, replace=False)
+        s_path = nx.dijkstra_path(net, s, d)
+        source.append(s)
+        destin.append(d)
+        paths.append(s_path)
+    return source, destin, paths
 
-net = nx.from_numpy_matrix(w)
-paths = []
+def vxi_1(l, U):
+    V = 1 / (1 + np.exp(-1 * l * U))
+    np.fill_diagonal(V,0)
+    return V
+
+def delta_kronecker(a, b):
+    if a == b:
+        return 1
+    else:
+        return 0
+
+def ixi(u1, u2, u4, u5, C_xi, rho, s, d):
+    for x in range(I.shape[0]):
+        for i in range(I.shape[1]):
+            if x == i:
+                I[x,i] = 0
+            else:
+                I[x,i] = - (u1 / 2) * C_xi[x, i] * (1 - delta_kronecker(x, d) * delta_kronecker(i, s)) - (u2 / 2) * rho[x, i] * (1 - delta_kronecker(x, d) * delta_kronecker(i, s)) - (u4 / 2) + (u5 / 2) * (delta_kronecker(x, d) * delta_kronecker(i, s))
+    return I
+
+C_xi = np.array([[0,    0.91, 0.36, 0,    0,     0,    1.2,   0   ],
+                 [0.91, 0,    0,    37.5, 0,     0,    0,     1.02],
+                 [0.36, 0,    0,    0.47, 0.64,  0,    0,     0   ],
+                 [0,    37.5, 0.47, 0,    0,     0.5,  0,     0   ],
+                 [0,    0,    0.64, 0,    0,     0.56, 0.425, 0   ],
+                 [0,    0,    0,    0.5,  0.56,  0,    0,     0.4 ],
+                 [1.2,  0,    0,    0,    0.425, 0,    0,     1.1 ],
+                 [0,    1.02, 0,    0,    0,     0.4,  1.1,   0   ]])
+
+net = nx.from_numpy_matrix(C_xi)
 paquetes = 10
+source, destin, paths = dijkstra_sp(net, paquetes, C_xi)
 
-for i in range(paquetes):
-    rng = np.random.default_rng()
-    source, destin = rng.choice(w.shape[0], size=2, replace=False)
-    s_path = nx.dijkstra_path(net, source, destin)
-    paths.append(s_path)
-
-print(w)
+print(C_xi)
 print(paths)
 
 u1 = 950
@@ -27,10 +56,19 @@ u2 = 2500
 u3 = 1500
 u4 = 475
 u5 = 2500
-A = 0.0001
-B = 0.00001
-C = 0.00001
-l = 1
+A = 0.0057
+B = 0.0072
+C = 0.0064
+l = 6
+
+rho = np.where(C_xi == 0, 1, 0)
+#print(rho)
+
+U = np.zeros(C_xi.shape)
+I = np.zeros(C_xi.shape)
+
+V = vxi_1(U, l)
+I = ixi(u1, u2, u4, u5, C_xi, rho, source[0], destin[0])
 
 #nx.draw(net, with_labels = True)           # graficar red
 #plt.show()
