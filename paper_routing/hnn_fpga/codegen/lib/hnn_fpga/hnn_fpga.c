e5ed787fdb4d7b809e17a475ef348bec3ea981c7
agustinsilva447 <agustinsilva447@gmail.com>
2021-07-27 15:39:26 -0300

    hnn fpga

--- paper_routing/hnn_fpga/codegen/lib/hnn_fpga/hnn_fpga.c
+++ paper_routing/hnn_fpga/codegen/lib/hnn_fpga/hnn_fpga.c
@@ -1,25 +1,29 @@
 #include "hnn_fpga.h"
 #include <math.h>
 
-void hnn_fpga(const float U[64], float l, float V[64])
+#define N1 8    //Cantidad de nodos
+
+void hnn_fpga(const float U[N1 * N1], float l, float V[N1 * N1])
 {
 #pragma HLS INTERFACE s_axilite port=V
 #pragma HLS INTERFACE s_axilite port=l
 #pragma HLS INTERFACE s_axilite port=U
 #pragma HLS INTERFACE s_axilite port=return
-  int x;
-  int i;
-  int V_tmp;
-  for (x = 0; x < 8; x++) {
-#pragma HLS UNROLL
-    for (i = 0; i < 8; i++) {
-#pragma HLS UNROLL
-      if (x == i) {
-        V[(x << 3) + i] = 0.0;
-      } else {
-        V_tmp = (x << 3) + i;
-        V[V_tmp] = 1.0 / (exp(-U[V_tmp] * l) + 1.0);
-      }
-    }
-  }
+
+	int x;
+	int i;
+	int V_tmp;
+
+	for (x = 0; x < N1; x++) {
+	#pragma HLS UNROLL
+		for (i = 0; i < N1; i++) {
+		#pragma HLS UNROLL
+			V_tmp = x * N1 + i;
+			if (x == i) {
+				V[V_tmp] = 0;
+			} else {
+				V[V_tmp] = 1 / (1 + exp(-U[V_tmp] * l));
+			}
+		}
+	}
 }
