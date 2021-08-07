3c368715ee63ef05f898790be3bbdfbcae873b9e
agustinsilva447 <agustinsilva447@gmail.com>
2021-08-08 00:40:11 +0200

    hnn_spp

--- paper_routing/scripts/plot_HNN_Zynq.py
+++ paper_routing/scripts/plot_HNN_Zynq.py
@@ -38,16 +38,27 @@ laten = np.array([         37,    41,    47,    55,    65,    77,    91,    107,
 lut   = np.array([         3113,  3391,  3714,  4165,  4675,  5267,  5973,  6801,  7688,  8741,  9879, 
                     11059, 12343, 13719, 15353, 17091, 18761, 20529, 22400, 24364, 26429, 28393, 29954, 
                     31538, 33191, 34907, 36692, 38540, 40457, 42440, 44486, 46601, 48782, 51029, 53339])  
-"""                    
+
 laten = np.array([         64,    68,    74,    82,    92,    104,   118,   134,   152,   172,   194,  
                     218,   244,   272,   302,   334,   368,   404,   442,   482,   524,   568,   614,
                     662,   712,   764,   818,   874,   992,   1054,  1118,  1184])
                     
 lut   = np.array([         8911,  9189,  9521,  10025, 10559, 11163, 11869, 12713, 13607, 14623, 15761, 
                     16941, 18219, 19675, 21229, 22961, 24631, 26399, 28265, 30229, 32235, 34147, 35705,
-                    37289, 38938, 40655, 42437, 44285, 48179, 50225, 52337, 54515])                    
+                    37289, 38938, 40655, 42437, 44285, 48179, 50225, 52337, 54515])      
+"""                    
+laten = np.array([                                                   118,   134,   152,   172,   194,  
+                    218,   244,   272,   302,   334,   368,   404,   442,   482,   524,   568,   614,
+                    662,   712,   764,   818,   874,   992,   1054,  1118,  1184])
+                    
+lut   = np.array([                                                   11869, 12713, 13607, 14623, 15761, 
+                    16941, 18219, 19675, 21229, 22961, 24631, 26399, 28265, 30229, 32235, 34147, 35705,
+                    37289, 38938, 40655, 42437, 44285, 48179, 50225, 52337, 54515])      
+
+
 nodes_h = np.arange(2, len(laten)+2, 1)
 
+"""
 l = 6
 t_num = []
 t_sci = []
@@ -57,8 +68,7 @@ V = vxi_2(l, U)
 it = 10
 
 nodes_s = np.arange(2, 500, 1)
-
-for n1 in tqdm(nodes_h):
+for n1 in tqdm(nodes_s):
     t_ntot = 0
     t_stot = 0
     for n2 in range(it):
@@ -73,22 +83,25 @@ for n1 in tqdm(nodes_h):
         t_stot += (time() - t_0)
     t_num.append(t_ntot/it)
     t_sci.append(t_stot/it)
+"""
 
-fig, axs = plt.subplots(2, 2, figsize=(12, 12))
-axs[0, 0].plot(nodes_h, laten, "r-o")
-axs[0, 0].set_title("HNN FPGA Latency")
-#axs[0, 0].set_xlabel("Nodes (number)")
-axs[0, 0].set_ylabel("Latency (cycles)")
-axs[0, 1].plot(nodes_h, lut, "g-o")
-axs[0, 1].set_title("HNN FPGA LUTs")
-#axs[0, 1].set_xlabel("Nodes (number)")
-axs[0, 1].set_ylabel("LUTs (number)")
-axs[1, 0].plot(nodes_h, t_num, "b-o")
+fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+axs[0].plot(nodes_h, lut, "b-o")
+axs[0].set_title("HNN FPGA LUTs")
+axs[0].set_xlabel("Nodes (number)")
+axs[0].set_ylabel("LUTs (number)")
+axs[1].plot(nodes_h, laten, "r-o")
+axs[1].set_title("HNN FPGA Latency")
+axs[1].set_xlabel("Nodes (number)")
+axs[1].set_ylabel("Latency (cycles)")
+"""
+axs[1, 0].plot(nodes_s, t_num, "b-o")
 axs[1, 0].set_title("HNN Numpy")
 axs[1, 0].set_xlabel("Nodes (number)")
 axs[1, 0].set_ylabel("Time")
-axs[1, 1].plot(nodes_h, t_sci, "c-o")
+axs[1, 1].plot(nodes_s, t_sci, "c-o")
 axs[1, 1].set_title("HNN Scipy")
 axs[1, 1].set_xlabel("Nodes (number)")
 axs[1, 1].set_ylabel("Time")
+"""
 plt.show()
