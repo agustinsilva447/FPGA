d92166e4e812a0be923063a5842794ba644f7e54
agustinsilva447 <agustinsilva447@gmail.com>
2021-08-11 19:03:05 +0200

    ultrascale ps

---
+++ ultrascale/zynq_uscale_plus_lab21/zynq_uscale_plus_lab21.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+//Date        : Wed Aug 11 17:40:20 2021
+//Host        : agustinsilva447-Lenovo-G50-80 running 64-bit Ubuntu 20.04.2 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   ();
+
+
+  design_1 design_1_i
+       ();
+endmodule
---
+++ ultrascale/zynq_uscale_plus_lab21/zynq_uscale_plus_lab21.srcs/sources_1/bd/design_1/sim/design_1.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+//Date        : Wed Aug 11 17:40:19 2021
+//Host        : agustinsilva447-Lenovo-G50-80 running 64-bit Ubuntu 20.04.2 LTS
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,da_zynq_ultra_ps_e_cnt=1,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+module design_1
+   ();
+
+
+  design_1_zynq_ultra_ps_e_0_0 zynq_ultra_ps_e_0
+       (.pl_ps_irq0(1'b0));
+endmodule
