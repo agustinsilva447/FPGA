b497d0c30e31c50bbc41f40ebb37e12aaf377571
unknown <mlabadm@hp6g4-mlab-2.ictp.it>
2021-08-20 20:15:54 +0200

    nqueen

---
+++ nqueens/nqueens_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+n = int(input("Ingrese el tamano del tablero: "))
+a = np.zeros([1,n])[0]
+n_sol = 0
+
+a[0] = 2
+for k in np.arange(1,n,1):
+    for u in np.arange(1,n+1,1):
+        for j in range(k+1):
+            if (u==a[j]) or (np.abs(u-a[j])==(k-j)):
+                break
+            if (j==k):
+                a[k] = u
+    print(a)
