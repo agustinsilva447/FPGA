6c9ccef76fc14339454eb39bb6d5bfa386dabc3a
agustinsilva447 <agustinsilva447@gmail.com>
2021-08-26 15:39:31 +0200

    nqueens fpga

--- nqueens/nqueens/solution1/script.tcl
+++ nqueens/nqueens/solution1/script.tcl
@@ -6,10 +6,11 @@
 open_project nqueens
 set_top nqueens
 add_files nqueens/nqueens.cpp
-add_files -tb nqueens/nqueens_tb.cpp
+add_files -tb nqueens/nqueens_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution1"
 set_part {xczu9eg-ffvb1156-2-e}
 create_clock -period 10 -name default
+config_export -format ip_catalog -rtl vhdl
 #source "./nqueens/solution1/directives.tcl"
 csim_design
 csynth_design
