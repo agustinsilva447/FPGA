f51fde9c957aa993939851de02df999c402aab61
agustinsilva447 <agustinsilva447@gmail.com>
2021-10-15 17:08:26 +0200

    cellular automata data

---
+++ tsp/automatas.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+f = open ('numeros2.txt','r')
+text = f.read()
+f.close()
+
+text = os.linesep.join([s for s in text.splitlines() if s])
+text = [np.binary_repr(int(s), width=32) for s in text.splitlines()]
+text = "".join(text)
+
+#print(text)
+
+text_file = open("binario_data_golden.txt", "wt")
+n = text_file.write(text)
+text_file.close()
