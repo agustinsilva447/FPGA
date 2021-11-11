9879a0dd8f98164df32d1a7164b4a075e18d1430
agustinsilva447 <agustinsilva447@gmail.com>
2021-11-11 21:39:15 +0100

    ACA dataset

--- tsp/automatas.py
+++ tsp/automatas.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-f = open ('numeros2.txt','r')
+f = open ('/home/agustinsilva447/Descargas/numeros_data_golden.txt','r')
 text = f.read()
 f.close()
 
@@ -9,8 +9,8 @@ text = os.linesep.join([s for s in text.splitlines() if s])
 text = [np.binary_repr(int(s), width=32) for s in text.splitlines()]
 text = "".join(text)
 
-#print(text)
+print(text)
 
-text_file = open("binario_data_golden.txt", "wt")
-n = text_file.write(text)
-text_file.close()
+#text_file = open("binario_data_golden.txt", "wt")
+#n = text_file.write(text)
+#text_file.close()
---
+++ tsp/espacial.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open ('/home/agustinsilva447/Descargas/numeros_data_golden.txt','r')
+text = f.read()
+f.close()
+
+text = os.linesep.join([s for s in text.splitlines() if s])
+text = np.array([int(s) for s in text.splitlines()])
+
+plt.hist(text, bins=1024)
+plt.show()
